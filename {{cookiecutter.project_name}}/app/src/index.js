27ad0f21d5d7593a7e9737e12cfc4dfaca7b03a1
Peter Halliday <pghalliday@gmail.com>
2017-04-02 19:13:37 +0200

    Update index.js

--- {{cookiecutter.project_name}}/app/src/index.js
+++ {{cookiecutter.project_name}}/app/src/index.js
@@ -5,7 +5,9 @@ import {
 } from 'redux';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise';
-import createLogger from 'redux-logger';
+import {
+  createLogger,
+} from 'redux-logger';
 import {
   composeWithDevTools,
 } from 'redux-devtools-extension';
